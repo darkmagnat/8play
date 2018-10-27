cba68d14340d75a90516e7f3866a71a9911bcfc9
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-10-27 17:06:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -369,6 +369,29 @@ if (message.content.startsWith(adminprefix + 'st')) {
 });
 
 
+const devs = ['383711936174620672' , '383711936174620672'];
+const adminprefix = ".";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/حب بلا حدود");
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  }
+  });
 
 
 client.login(process.env.BOT_TOKEN);
